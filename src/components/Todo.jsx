b70d0ed04e8467b9192349d444d76f3adbc3606e
user.email <99192547+not-iron-yury@users.noreply.github.com>
2024-07-19 16:01:16 +0300

    Отправка задачи в массив
    Changes to be committed:
    	modified:   src/components/Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,23 +2,29 @@ import { useState } from "react";
 
 export default function Todo() {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  const handleClick = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit} id='formElem'>
         <input
           type='text'
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
+          name='inputitem'
         />
-        <button type='submit' onClick={handleClick}>
-          +
-        </button>
+        <button type='submit'>+</button>
       </form>
+      <p>{String(todos)}</p>
     </div>
   );
 }
